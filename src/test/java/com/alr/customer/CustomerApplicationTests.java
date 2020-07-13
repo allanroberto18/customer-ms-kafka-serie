012a08c5ffca42365f9c01e42fa7b9219a22f69b
allan roberto <allanroberto18@gmail.com>
2020-07-14 00:12:47 +0200

    set test active profiler on CustomerApplicationTests

--- src/test/java/com/alr/customer/CustomerApplicationTests.java
+++ src/test/java/com/alr/customer/CustomerApplicationTests.java
@@ -11,10 +11,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.function.Function;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CustomerApplicationTests {
 
   @Autowired
